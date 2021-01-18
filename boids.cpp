553cc3110bc7a9b5a3658ada346d44ea110db0bb
Charlie Winter <charliewinter.vfx@gmail.com>
2021-01-18 13:36:32 -0800

    channging default data passing to struct

--- boids.cpp
+++ boids.cpp
@@ -15,9 +15,9 @@ int boids::init(AttributeData attributes)
 
 	// Set Model Parameters
 	modules.back().run_method("set_attrs", attributes.count, attributes.width,
-							attributes.height, attributes.depth, attributes.min_dist,
-							attributes.max_dist, attributes.vel_mult, attributes.min_vel,
-							attributes.max_vel, attributes.maxacc);
+		attributes.height, attributes.depth, attributes.min_dist,
+		attributes.max_dist, attributes.vel_mult, attributes.min_vel,
+		attributes.max_vel, attributes.maxacc);
 
 	// Place Model on GPU, Set to Eval Mode
 	modules.back().to(torch::kCUDA);
@@ -44,18 +44,18 @@ bool boids::run(float* pos, float * vel, TickData tick_attrs)
 	{
 		// Make GPU Tensor For Goal and Avoidance Targets
 		auto goal = torch::tensor({ tick_attrs.gx,
-									tick_attrs.gy,
-									tick_attrs.gz }).to(torch::kCUDA);
+			tick_attrs.gy,
+			tick_attrs.gz }).to(torch::kCUDA);
 
 		auto avoid = torch::tensor({ tick_attrs.ax,
-									tick_attrs.ay,
-									tick_attrs.az }).to(torch::kCUDA);
+			tick_attrs.ay,
+			tick_attrs.az }).to(torch::kCUDA);
 
 		// Run Model
 		auto output = modules[tick_attrs.ticket].run_method("forward", goal,
-															avoid, tick_attrs.t_mult,
-															tick_attrs.a_mult,
-															tick_attrs.a_dist);
+			avoid, tick_attrs.t_mult,
+			tick_attrs.a_mult,
+			tick_attrs.a_dist);
 
 		// Copy Result to GPU
 		auto position = output.toTuple()->elements()[0].toTensor().detach().to(torch::kCPU);;
@@ -63,10 +63,10 @@ bool boids::run(float* pos, float * vel, TickData tick_attrs)
 
 		// Copy Tensor Data Into Pointer
 		std::memcpy(pos, position.contiguous().data<float>(),
-						sizeof(float) *position.numel());
+			sizeof(float) *position.numel());
 
 		std::memcpy(vel, velocity.contiguous().data<float>(),
-						sizeof(float) *velocity.numel());
+			sizeof(float) *velocity.numel());
 	}
 
 	catch (const c10::Error& e) 
